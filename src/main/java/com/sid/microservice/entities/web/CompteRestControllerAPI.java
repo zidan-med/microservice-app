3ea8b410a12bc3bf3ea1df73a2a444c31b4e992e
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-02-18 13:51:07 +0100

    implementation web service avec RestFull

---
+++ src/main/java/com/sid/microservice/entities/web/CompteRestControllerAPI.java
@@ -0,0 +1,45 @@
+package com.sid.microservice.entities.web;
+
+import com.sid.microservice.entities.Compte;
+import com.sid.microservice.repositories.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+//Rest controller API
+@RestController
+//url(path) de base
+@RequestMapping("/banque")
+public class CompteRestControllerAPI {
+    //injection dependence
+    @Autowired
+    private CompteRepository compteRepository;
+    @GetMapping(path = "/comptes",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+    public List<Compte> compteList(){
+        return compteRepository.findAll();
+    }
+    @GetMapping("/comptes/{id}")
+    public Compte getOne(@PathVariable(value = "id") Long id){
+
+        return compteRepository.findById(id).get();
+    }
+    @PostMapping("/comptes")
+    public Compte save(@RequestBody Compte compte){
+
+        return compteRepository.save(compte);
+    }
+    @PutMapping("/comptes/{id}")
+    public Compte update(@RequestBody// Compte compte,@PathVariable(value = "id") Long id){
+        compte.setId(id);
+        return compteRepository.save(compte);
+    }
+    @DeleteMapping("/comptes/{id}")
+    public void delete(@PathVariable(value = "id") Long id){
+
+        compteRepository.deleteById(id);
+    }
+}
